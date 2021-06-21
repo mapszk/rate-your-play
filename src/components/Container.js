7c2cbcb05f9a85bbd57524c752e2383eb283a9fb
Martin Pilaszek <martinpilaszek@gmail.com>
2021-06-20 21:19:15 -0300

    footer agregado

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
+import Logo from '../components/Logo'
+import { AiFillInstagram, AiFillFacebook } from 'react-icons/ai'
+import { IconContext } from 'react-icons'
+import { useHistory } from 'react-router';
+
 const index = ({children}) => {
     return (
-        <div className="w-11/12 max-w-screen-lg mx-auto">
+        <div className="w-11/12 max-w-screen-lg mx-auto text-primary">
             {children}
+            <footer className="my-5 flex justify-between items-center py-2">
+                <Logo width="2.5rem" primary />
+                <div className="flex">
+                    <a rel="noopener noreferer" target="_blank" href="https://www.instagram.com">
+                        <IconContext.Provider value={{size: 25}}>
+                            <AiFillFacebook />
+                        </IconContext.Provider>
+                    </a>
+                    <div className="ml-3">
+                        <a rel="noopener noreferer" target="_blank" href="https://www.facebook.com">
+                            <IconContext.Provider value={{size: 25}}>
+                                <AiFillInstagram/>
+                            </IconContext.Provider>
+                        </a>
+                    </div>
+                </div>
+            </footer>
         </div>
     );
 }
